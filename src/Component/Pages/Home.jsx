3aceaebeb282c1a10f2ac7eecb6a85325440520b
Malaywebdeveloper <125017719+Malaywebdeveloper@users.noreply.github.com>
2023-06-04 00:06:28 +0530

    import work

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../Layout/Layout";
 import { Link } from "react-router-dom";
 import Banner from "../Image/banner.jpeg";
+import "../Style/HomeStyle.css";
 const Home = () => {
   return (
     <Layout>
